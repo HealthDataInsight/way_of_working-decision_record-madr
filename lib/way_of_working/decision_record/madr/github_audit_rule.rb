0895395702bd065df8821264d0f84b2b81e7abfc
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-10-15 23:16:15 +0100

    Fixed the GithubAuditRule to detect stale documentation

--- lib/way_of_working/decision_record/madr/github_audit_rule.rb
+++ lib/way_of_working/decision_record/madr/github_audit_rule.rb
@@ -10,6 +10,7 @@ module WayOfWorking
       class GithubAuditRule < ::WayOfWorking::Audit::Github::Rules::Base
         ADR_TEMPLATE_PATH = 'docs/decisions/adr-template.md'
         DECISION_ZERO_PATH = 'docs/decisions/0000-use-markdown-any-decision-records.md'
+        WOW_DOCUMENTATION_PATH = 'docs/way_of_working/decision-records.md'
 
         source_root ::WayOfWorking::DecisionRecord::Madr.source_root
 
@@ -17,6 +18,11 @@ module WayOfWorking
           @errors << 'No MADR Decision Record template found' unless repo_file_contains?(
             ADR_TEMPLATE_PATH, '## Context and Problem Statement'
           )
+
+          unless repo_file_contains_source_file?(WOW_DOCUMENTATION_PATH)
+            @errors << 'Stale or missing documentation for using MADR Decision Records'
+          end
+
           return if repo_file_contains_source_file?(DECISION_ZERO_PATH)
 
           @errors << 'No 0000-use-markdown-any-decision-records.md Decision Record found'
