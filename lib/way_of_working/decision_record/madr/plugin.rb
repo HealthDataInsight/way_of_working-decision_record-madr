9741613709c8ea63f3a9dcb2f105bff06c872ba6
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-11-21 14:34:48 +0000

    Removed Zeitwerk ignore to make it happier

--- lib/way_of_working/decision_record/madr/plugin.rb
+++ lib/way_of_working/decision_record/madr/plugin.rb
@@ -1,1 +1,13 @@
 require 'way_of_working/decision_record/madr'
+
+# Add any plug specific code here
+
+module WayOfWorking
+  module DecisionRecord
+    module Madr
+      # This makes zeitwerk happy, which makes us happy too
+      module Plugin
+      end
+    end
+  end
+end
